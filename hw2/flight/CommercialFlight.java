f5b1a5f25fd896ef8fa57f39cd81061943aefa6e
David Gripman <dgripman9@gmail.com>
2022-04-25 13:51:04 -0600

    Homework 2

---
+++ hw2/flight/CommercialFlight.java
@@ -0,0 +1,112 @@
+package HW1.flight;
+
+import HW1.airline.Airline;
+import HW1.airport.Airport;
+import HW1.ExceptionHandlings.NullParameterException;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+
+public class Flight {
+    private Airline airline;
+    private Airport origin;
+    private Airport destination;
+    private UUID flightNumber;
+    private Date departureTime;
+
+
+    public Flight(Airline airline, Airport origin, Airport destination, UUID flightNumber, Date departureTime) throws NullParameterException {
+        this.airline = airline;
+        this.origin = origin;
+        this.destination = destination;
+        this.flightNumber = UUID.randomUUID();
+        this.departureTime = new Date();
+    }
+
+    public String getFlightNumber() {
+        return flightNumber.toString();
+    }
+
+    public void setFlightNumber() {
+        this.flightNumber = UUID.randomUUID();
+    }
+
+    public String getDepartureTime() {
+        return departureTime.toString();
+    }
+
+    public void setDepartureTime() {
+        this.departureTime = new Date();
+    }
+
+    public String getAirline() {
+        return airline.toString();
+    }
+
+    public void setAirline(Airline airline) throws NullParameterException {
+        if (airline == null) {
+            throw new NullParameterException("Airline cannot be null");
+        }
+        this.airline = airline;
+    }
+
+    public String getOrigin() {
+        return origin.toString();
+    }
+
+    public void setOrigin(Airport origin) throws NullParameterException {
+        if (origin == null) {
+            throw new NullParameterException("Origin cannot be null");
+        }
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination.toString();
+    }
+
+    public void setDestination(Airport destination) throws NullParameterException {
+        if (destination == null) {
+            throw new NullParameterException("Destination cannot be null");
+        }
+        this.destination = destination;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return airline.equals(flight.airline) && origin.equals(flight.origin) && destination.equals(flight.destination) && flightNumber.equals(flight.flightNumber) && departureTime.equals(flight.departureTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(airline, origin, destination, flightNumber, departureTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "airline=" + airline +
+                ", origin=" + origin +
+                ", destination=" + destination +
+                ", flightNumber=" + flightNumber +
+                ", departureTime=" + departureTime +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
